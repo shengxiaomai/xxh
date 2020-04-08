63adb9c109cc3085f0366110585cec8cf58336d0
shengxiaomai <734978582@qq.com>
2020-04-08 19:12:40 +0800

    封装shell 修改run执行

--- runnerByAllure.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import subprocess
-
-if __name__== '__main__':
-
-    #pytest.main(['--alluredir','/Users/xiaomai/PycharmProjects/shy/Report/xml','/Users/xiaomai/PycharmProjects/shy/TestCase'])
-    pytest.main(['--alluredir', '/Users/xiaomai/PycharmProjects/shyApi/Report/xml', '--allure-severities=blocker','/Users/xiaomai/PycharmProjects/shyApi/TestCase'])
-    print(subprocess.getstatusoutput('allure generate   --clean /Users/xiaomai/PycharmProjects/shyApi/Report/xml --output /Users/xiaomai/PycharmProjects/shyApi/Report/html'))
