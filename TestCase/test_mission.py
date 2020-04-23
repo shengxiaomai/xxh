0ba222f6cde5eeeae2f52ebcb7b1f2a9d06485ac
chanshuze <1127359388@qq.com>
2020-04-23 16:49:25 +0800

    增加用户加入学习会接口 增加用户退出学习会接口

--- TestCase/test_mission.py
+++ TestCase/test_mission.py
@@ -17,6 +17,29 @@ class TestMission:
 
     @allure.severity('normal')
     @allure.story('Collections')
+    @pytest.mark.parametrize("payload",
+                             [
+                                 {"name": "自由讨论",
+                                  "desc": "sdfgdsfgfdsg",
+                                  "ext": "{\"tagId\":\"626549294551269829\",\"meetingType\":1,\"timeUse\":\"5分钟\"}",
+                                  "tagId": "626549294551269829"},
+                                 {"name": "主题演讲",
+                                  "desc": "<!DOCTYPE html>\n      <html lang=\"en\">\n      <head>\n        <meta charset=\"UTF-8\">\n        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n        <title>Document</title>\n        <style>\n          * {\n            padding: 0;\n            margin: 0;\n          }\n        </style>\n      </head>\n      <body>\n        <div style='font-size: 17px;color: #7f7f7f;'>461616</div>\n      </body>\n      </html>",
+                                  "ext": "{\"tagId\":\"626544899323331013\",\"meetingType\":0,\"speaker\":\"15166841990 Qiucw\",\"speakerId\":\"636786270315479359\",\"timeUse\":\"9分钟\",\"teachStyle\":\"直播演讲\",\"audio\":{\"result\":\"\",\"name\":\"\"},\"video\":{\"result\":\"\",\"name\":\"\"},\"content\":\"<!DOCTYPE html>\\n      <html lang=\\\"en\\\">\\n      <head>\\n        <meta charset=\\\"UTF-8\\\">\\n        <meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\">\\n        <title>Document</title>\\n        <style>\\n          * {\\n            padding: 0;\\n            margin: 0;\\n          }\\n        </style>\\n      </head>\\n      <body>\\n        <div style='font-size: 17px;color: #7f7f7f;'>461616</div>\\n      </body>\\n      </html>\"}",
+                                  "tagId": "626544899323331013"}
+                             ])
+    def test_createAgenda(self, payload):
+        '''
+        添加议程
+        版本v1.0.0
+        :param payload:
+        :return:
+        '''
+        self.log.info("开始测试添加议程")
+        a = missionApi.createMeeting(payload)
+        assert a["msg"] == self.expected_msg
+
+
     @pytest.mark.parametrize("payload", [{
             "desc": "学习会议的文稿",
             "name": "新建的学习会",
@@ -24,7 +47,8 @@ class TestMission:
     }])
     def test_createMeeting(self,payload):
         '''
-        测试创建学习会
+        用户创建学习会
+        版本v1.0.0
         :param payload: 参数
         :return:
         '''
@@ -33,35 +57,135 @@ class TestMission:
         assert a["msg"] == self.expected_msg
 
 
-
-    @pytest.mark.parametrize("payload",
-                    [
-                        {"name": "自由讨论", "desc": "sdfgdsfgfdsg",
-                   "ext": "{\"tagId\":\"626549294551269829\",\"meetingType\":1,\"timeUse\":\"5分钟\"}",
-                   "tagId": "626549294551269829"},
-                     {"name":"主题演讲", "desc":"<!DOCTYPE html>\n      <html lang=\"en\">\n      <head>\n        <meta charset=\"UTF-8\">\n        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n        <title>Document</title>\n        <style>\n          * {\n            padding: 0;\n            margin: 0;\n          }\n        </style>\n      </head>\n      <body>\n        <div style='font-size: 17px;color: #7f7f7f;'>461616</div>\n      </body>\n      </html>","ext":"{\"tagId\":\"626544899323331013\",\"meetingType\":0,\"speaker\":\"15166841990 Qiucw\",\"speakerId\":\"636786270315479359\",\"timeUse\":\"9分钟\",\"teachStyle\":\"直播演讲\",\"audio\":{\"result\":\"\",\"name\":\"\"},\"video\":{\"result\":\"\",\"name\":\"\"},\"content\":\"<!DOCTYPE html>\\n      <html lang=\\\"en\\\">\\n      <head>\\n        <meta charset=\\\"UTF-8\\\">\\n        <meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\">\\n        <title>Document</title>\\n        <style>\\n          * {\\n            padding: 0;\\n            margin: 0;\\n          }\\n        </style>\\n      </head>\\n      <body>\\n        <div style='font-size: 17px;color: #7f7f7f;'>461616</div>\\n      </body>\\n      </html>\"}",
-                                                   "tagId":"626544899323331013"}
-                    ])
-    def test_createAgenda(self,payload):
-        '''
-        测试添加议程
-        :param payload:
-        :return:
-        '''
-        self.log.info("开始测试添加议程")
-        a = missionApi.createMeeting(payload)
-        assert a["msg"] == self.expected_msg
-
-    @pytest.mark.parametrize("payload", [{"agendaIds":["656964581662392753"],"name":"你好","pid":"656789803471208874","startTime":datetime.datetime.strftime(datetime.datetime.now()+datetime.timedelta(minutes=5),'%Y-%m-%d %H:%M:%S')}])
+    @pytest.mark.parametrize("payload", [{"agendaIds":["666910263752000147"],
+                                          "content":"反反复复个广告发个广告广告方法个 v 尺寸",
+                                          "desc": "反反复复个广告发个广告广告方法个 v 尺寸",
+                                          "ext": "{\"video\":{\"result\":\"\",\"name\":\"\"},\"audio\":{\"result\":\"\",\"name\":\"\"},\"content\":{\"contentHtml\":\"\",\"contentText\":\"\"}}",
+                                          "name": "测试",
+                                          "pid":"666892345685312147",
+                                          "startTime":datetime.datetime.strftime(datetime.datetime.now()+datetime.timedelta(minutes=5),'%Y-%m-%d %H:%M:%S')}])
     def test_createTheme(self,payload):
         '''
-        测试添加主题
+        添加主题
+        版本v1.0.0
         :param payload:
         :return:
         '''
         self.log.info("开始测试添加主题")
         a = missionApi.createTheme(payload)
         assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{"desc": "再次编辑",
+    "name": "测试编辑学习会",
+    "content":"再次编辑",
+    "id":"667115786527048181"}])
+    def test_editMeeting(self,payload):
+        '''
+        编辑我的学习会
+        版本v1.1.0
+        :param payload: 参数
+        :return:
+        '''
+        self.log.info("编辑我的学习会")
+        a = missionApi.editMeeting(payload)
+        assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{"agendaIds":["667150077277504157"],
+                                          "content":"再次修改主题会议内容",
+                                          "desc": "再次修改主题会议内容",
+                                          "ext": "{\"video\":{\"result\":\"\",\"name\":\"\"},\"audio\":{\"result\":\"\",\"name\":\"\"},\"content\":{\"contentHtml\":\"\",\"contentText\":\"\"}}",
+                                          "id": "667150233641157277",
+                                          "name": "测试编辑主题会",
+                                          "pid":"666387367187186323",
+                                          "startTime":"2020-04-25 23:00:00"}])
+    def test_editTheme(self,payload):
+        '''
+        编辑主题内容
+        版本v1.1.0
+        :param payload:
+        :return:
+        '''
+        self.log.info("开始编辑主题内容")
+        a = missionApi.editTheme(payload)
+        assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{
+        "idx": 0,
+        "size": 20
+    }])
+    def test_getMeetingDetail(self, payload):
+        '''
+        学习会详情
+        版本v1.1.0
+        :param payload: 参数
+        :return:
+        '''
+        self.log.info("学习会详情")
+        a = missionApi.getMeetingDetail(payload)
+        assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{}])
+    def test_getMeetingList(self, payload):
+        '''
+        用户查询所有学习会列表
+        版本v1.1.0
+        :param payload: 参数
+        :return:
+        '''
+        self.log.info("用户查询所有学习会列表")
+        a = missionApi.getMeetingList(payload)
+        assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{
+        "idx": 0,
+        "size": 20
+    }])
+    def test_getMyMeetingList(self, payload):
+        '''
+        用户查看我的会议列表
+        版本v1.1.0
+        :param payload: 参数
+        :return:
+        '''
+        self.log.info("用户查看我的会议列表")
+        a = missionApi.getMyMeetingList(payload)
+        assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{
+        "idx": 0,
+        "size": 20,
+        "userId": "636804209588568383"
+    }])
+    def test_missionThemeList(self, payload):
+        '''
+        任务主题列表查询(首页列表)
+        版本v1.0.0
+        :param payload: 参数
+        :return:
+        '''
+        self.log.info("任务主题列表查询(首页列表)")
+        a = missionApi.missionThemeList(payload)
+        assert a["msg"] == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{}])
+    def test_queryThemeInfo(self, payload):
+        '''
+        用户查询所有学习会列表
+        版本v1.1.0
+        :param payload: 参数
+        :return:
+        '''
+        self.log.info("用户查询所有学习会列表")
+        a = missionApi.queryThemeInfo(payload)
+        assert a["msg"] == self.expected_msg
+
 
     @pytest.mark.parametrize("payload", [{
         "missionId": "666387367187186323",
@@ -70,12 +194,14 @@ class TestMission:
     def test_userAddMission(self, payload):
         '''
         用户加入学习会
+        版本v1.0.0
         :param payload: 参数
         :return:
         '''
         self.log.info("用户加入学习会")
         a = missionApi.userAddMission(payload)
         assert a["msg"] == self.expected_msg
+
 
     @pytest.mark.parametrize("payload", [{
         "missionId": "666387367187186323",
@@ -84,6 +210,7 @@ class TestMission:
     def test_userExitMission(self, payload):
         '''
         用户退出学习会
+        版本v1.1.0
         :param payload: 参数
         :return:
         '''
--- api/mission.py
+++ api/mission.py
@@ -11,24 +11,38 @@ class missionApi(BaseApi):
 
     conf = Config()
     host = conf.host_debug
-    createMeeting_url = host+'createMeeting?t='+str(int(time.time()))
     createAgenda_url = host + 'createMeeting?t=' + str(int(time.time()))
+    createMeeting_url = host+'createMeeting?t='+str(int(time.time()))
     createTheme_url = host + 'createTheme?t=' + str(int(time.time()))
+    editMeeting_url = host + 'editMeeting?t=' + str(int(time.time()))
+    editTheme_url = host + 'editTheme?t=' + str(int(time.time()))
     userAddMission_url=host + 'userAddMission?t=' + str(int(time.time()))
     userExitMission_url = host + 'userExitMission?t=' + str(int(time.time()))
+    missionThemeList_url = host + 'missionThemeList?t=' + str(int(time.time()))
+    getMyMeetingList_url = host + 'getMyMeetingList?t=' + str(int(time.time()))
+    getMeetingDetail_url = host + 'getMeetingDetail/666387367187186323?t=' + str(int(time.time()))
+    getMeetingList_url = host + 'getMeetingList?t=' + str(int(time.time()))
+    queryThemeInfo_url = host + 'queryThemeInfo/667150233641157277?t=' + str(int(time.time()))
     headers = {"authorization": login.Login.login(), "content - type": "application/json;charset=utf-8"}
 
-    #创建学习会
-    @classmethod
-    def createMeeting(self,payload):
-        self.json_data = requests.post(self.createMeeting_url, headers=self.headers,json=payload, verify=False).json()
-        return self.json_data
+
+
+
     #添加议程
     @classmethod
     def createAgenda(self,payload):
         self.json_data = requests.post(self.createAgenda_url, headers=self.headers, json=payload, verify=False).json()
         self.verbose(self.json_data)
         return self.json_data
+
+
+    #创建学习会
+    @classmethod
+    def createMeeting(self,payload):
+        self.json_data = requests.post(self.createMeeting_url, headers=self.headers,json=payload, verify=False).json()
+        return self.json_data
+
+
     #创建主题会
     @classmethod
     def createTheme(self,payload):
@@ -40,15 +54,80 @@ class missionApi(BaseApi):
         self.json_data = requests.post(self.createTheme_url, headers=self.headers, json=payload, verify=False).json()
         self.verbose(self.json_data)
         return self.json_data
+
+
+    #编辑我的学习会
+    @classmethod
+    def editMeeting(self,payload):
+        self.json_data = requests.post(self.editMeeting_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #编辑主题内容
+    @classmethod
+    def editTheme(self,payload):
+        self.json_data = requests.post(self.editTheme_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #学习会详情
+    @classmethod
+    def getMeetingDetail(self,payload):
+        self.json_data = requests.get(self.getMeetingDetail_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #用户查询所有学习会列表
+    @classmethod
+    def getMeetingList(self,payload):
+        self.json_data = requests.post(self.getMeetingList_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #用户查看我的会议列表
+    @classmethod
+    def getMyMeetingList(self,payload):
+        self.json_data = requests.get(self.getMyMeetingList_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #任务主题列表查询(首页列表)
+    @classmethod
+    def missionThemeList(self,payload):
+        self.json_data = requests.post(self.missionThemeList_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #主题详情查询(包含议程)
+    @classmethod
+    def queryThemeInfo(self,payload):
+        self.json_data = requests.get(self.queryThemeInfo_url, headers=self.headers, json=payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
     #用户加入学习会
     @classmethod
     def userAddMission(self,payload):
         self.json_data = requests.post(self.userAddMission_url, headers=self.headers, json=payload, verify=False).json()
         self.verbose(self.json_data)
         return self.json_data
+
+
     #用户退出学习会
     @classmethod
     def userExitMission(self,payload):
         self.json_data = requests.post(self.userExitMission_url, headers=self.headers, json=payload, verify=False).json()
         self.verbose(self.json_data)
         return self.json_data
+
+
+
+
+
