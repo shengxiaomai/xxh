28b82147ce18c5f1a565d49ebaecce123be44a1b
chanshuze <1127359388@qq.com>
2020-04-27 20:41:18 +0800

    增加编辑用户信息接口 增加获取用户信息接口 增加根据手机号查询用户信息接口 增加修改音视频通话提示接口

--- TestCase/test_user.py
+++ TestCase/test_user.py
@@ -9,6 +9,8 @@ class TestUser:
 
     log = Log.MyLog()
     expected_msg = "success"
+    expected_mobile="15002020506"
+    expected_userId = "636804209588568383"
 
     @pytest.mark.parametrize("payload", [{"mobile": "18682115996"}])
     def test_sendVerifyCode(self,payload):
@@ -19,6 +21,7 @@ class TestUser:
         '''
         a=userApi(payload).sendVerifyCode()["msg"]
         assert a == self.expected_msg
+
 
     @pytest.mark.parametrize("payload", [{"device": "android","mobile": "15166831990","verifyCode": "1234"},{"device": "android","mobile": "15166831990","verifyCode": "2334"}])
     def test_sendVerifyCode(self, payload):
@@ -32,3 +35,53 @@ class TestUser:
             assert a == self.expected_msg
         else:
             assert a != self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{"isEdit":1,"sex":1}])
+    def test_editUserInfo(self, payload):
+        '''
+        编辑用户信息
+        :param payload:
+        :return:
+        '''
+        self.log.info("编辑用户信息")
+        a = userApi(payload).editUserInfo()["msg"]
+        assert a == self.expected_msg
+
+
+    @pytest.mark.parametrize("payload", [{}])
+    def test_getUserInfo(self, payload):
+        '''
+        获取用户信息
+        :param payload:
+        :return:
+        '''
+        self.log.info("获取用户信息")
+        a = userApi(payload).getUserInfo()["body"]["mobile"]
+        assert a == self.expected_mobile
+
+
+    @pytest.mark.parametrize("payload", [{"mobile":"15002020506"}])
+    def test_getUserInfoByMobile(self, payload):
+        '''
+        根据用户手机查询用户信息
+        :param payload:
+        :return:
+        '''
+        self.log.info("根据用户手机查询用户信息")
+        a = userApi(payload).getUserInfoByMobile()["body"]["userId"]
+        assert a == self.expected_userId
+
+
+    @pytest.mark.parametrize("payload", [{"editCode":2,"userId":"636804209588568383"}])
+    def test_editCallRemind(self, payload):
+        '''
+        修改语音视频通话提示(打开或者关闭用户语音和视频通话提醒)
+        :param payload:
+        :return:
+        '''
+        self.log.info("修改语音视频通话提示")
+        a = userApi(payload).editCallRemind()["body"]["userId"]
+        assert a == self.expected_userId
+
+
--- api/user.py
+++ api/user.py
@@ -13,14 +13,57 @@ class userApi(BaseApi):
         self.session = requests.session()
         self.sendVerifyCode_url = Config().user_host_debug+'sendVerifyCode'
         self.appLogin_url = Config().user_host_debug + 'appLogin'
+        self.editUserInfo_url = Config().user_host_debug + 'editUserInfo'
+        self.getUserInfo_url = Config().user_host_debug + 'getUserInfo'
+        self.getUserInfoByMobile_url = Config().user_host_debug + 'getUserInfoByMobile'
+        self.editCallRemind_url = Config().user_host_debug + 'editCallRemind'
+        self.headers = {"authorization": login.Login.login(), "content - type": "application/json;charset=utf-8"}
+
+
 
     def sendVerifyCode(self):
         self.json_data = self.session.post(self.sendVerifyCode_url,json=self.payload).json()
         self.verbose(self.json_data)
         return self.json_data
 
+
     def appLogin(self):
         self.json_data = self.session.post(self.appLogin_url, json=self.payload).json()
         self.verbose(self.json_data)
         return self.json_data
+
+
+    #编辑用户信息
+
+    def editUserInfo(self):
+        self.json_data = requests.post(self.editUserInfo_url, headers=self.headers, json=self.payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+    #获取用户信息
+
+    def getUserInfo(self):
+        self.json_data = requests.get(self.getUserInfo_url, headers=self.headers, json=self.payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #根据手机号查询用户信息
+
+    def getUserInfoByMobile(self):
+        self.json_data = requests.post(self.getUserInfoByMobile_url, headers=self.headers, json=self.payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+    #修改语音视频通话提示
+
+    def editCallRemind(self):
+        self.json_data = requests.post(self.editCallRemind_url, headers=self.headers, json=self.payload, verify=False).json()
+        self.verbose(self.json_data)
+        return self.json_data
+
+
+
+
 
